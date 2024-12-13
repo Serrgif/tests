29b6c92e299760f9b6260de6a09e5860823e2807
Irina <sergei32git@gmail.com>
2024-12-13 14:23:04 +0500

    dd

--- src/test/java/netology/tests/DebitBuyTest.java
+++ src/test/java/netology/tests/DebitBuyTest.java
@@ -1,18 +1,34 @@
 package netology.tests;
 
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import lombok.val;
 import netology.data.BdSqlHelper;
 import netology.data.DataHelper;
 import netology.pages.DebitBuy; // Убедитесь, что импортируете правильный класс
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DebitBuyTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
     @BeforeEach
     public void openPage() {
         open("http://localhost:8080");
