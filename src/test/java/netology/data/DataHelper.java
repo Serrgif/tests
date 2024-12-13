33b2cc5c39bb47ba0f9ad4ce2fa1af3b3f1ed753
Irina <sergei32git@gmail.com>
2024-12-13 15:13:12 +0500

    fdf

--- src/test/java/netology/data/DataHelper.java
+++ src/test/java/netology/data/DataHelper.java
@@ -3,6 +3,6 @@ package netology.data;
 public class DataHelper {
 
     public static Card getApprovedCard() {
-        return new Card("4444444444444441", "12", "25", "Dmitry Evdokimov", "123");
+        return new Card("4444444444444442", "12", "25", "Dmitry Evdokimov", "123");
     }
 }
